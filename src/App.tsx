6fcb93a9df657bc5250438e76432d1a67a6a1b50
George Diepiriye <georgediepiriye4u@gmail.com>
2023-04-24 02:37:28 +0100

    added todo list, edit and delete todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">Hello world</header>
+      <span className="heading">Tasky</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
